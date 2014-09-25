09d7b0ae5499ff914465e3073404c0c1782f05e1
Kevin Ross <kevin.ross@alienfast.com>
2014-09-25 17:17:50 -0500

    quick fix on test expectation.  Need to update the rest of the rspec assertions at some point.

--- spec/cucumber_statistics/renderer_spec.rb
+++ spec/cucumber_statistics/renderer_spec.rb
@@ -1,48 +1,50 @@
 require 'spec_helper'
 
-describe CucumberStatistics::Renderer do
+module CucumberStatistics
+  describe Renderer do
 
-  subject(:step_statistics) { StepStatistics.new }
-  subject(:overall_statistics) { OverallStatistics.new }
+    subject(:step_statistics) { StepStatistics.new }
+    subject(:overall_statistics) { OverallStatistics.new }
 
-  before(:each) do
-    # clean up before start, leave after in case we want to inspect it.
-    Configuration.clean_tmp_dir
+    before(:each) do
+      # clean up before start, leave after in case we want to inspect it.
+      Configuration.clean_tmp_dir
 
-    overall_statistics.start_time = Time.now - 35
-    overall_statistics.feature_count_inc
-    overall_statistics.scenario_count_inc
+      overall_statistics.start_time = Time.now - 35
+      overall_statistics.feature_count_inc
+      overall_statistics.scenario_count_inc
 
-    record 'my step 1', 1.100116
-    record 'my step 1', 9.213553
-    record 'my step 2', 3.100117
-    record 'my step 2', 14.204407
-    record 'my step 3', 1.500131
-    record 'my step 3', 3.48993
-    record 'my step 4', 4.21
-    record 'my step 4', 4.21
+      record 'my step 1', 1.100116
+      record 'my step 1', 9.213553
+      record 'my step 2', 3.100117
+      record 'my step 2', 14.204407
+      record 'my step 3', 1.500131
+      record 'my step 3', 3.48993
+      record 'my step 4', 4.21
+      record 'my step 4', 4.21
 
-    overall_statistics.end_time = Time.now
-    step_statistics.calculate
-  end
-
-
-  describe 'render_step_statistics' do
-    it 'should render content' do
-
-      File.exists?(Configuration.result_step_statistics).should be_false
-
-      absolute_file_name = Renderer.render_step_statistics step_statistics, overall_statistics
-      File.exists?(absolute_file_name).should be_true
-
-      #file = File.open(absolute_file_name, 'rb')
-      #file_contents = file.read
+      overall_statistics.end_time = Time.now
+      step_statistics.calculate
     end
-  end
 
-  def record(step_name, duration)
-    # fake a source for convenience
-    step_statistics.record step_name, duration, '/Users/kross/alienfast/acme/features/account management/admin_cancel_account.feature:8'
-    overall_statistics.step_count_inc
+
+    describe 'render_step_statistics' do
+      it 'should render content' do
+
+        expect(File.exists?(Configuration.result_step_statistics)).to eql false
+
+        absolute_file_name = Renderer.render_step_statistics step_statistics, overall_statistics
+        expect(File.exists?(absolute_file_name)).to eql true
+
+        #file = File.open(absolute_file_name, 'rb')
+        #file_contents = file.read
+      end
+    end
+
+    def record(step_name, duration)
+      # fake a source for convenience
+      step_statistics.record step_name, duration, '/Users/kross/alienfast/acme/features/account management/admin_cancel_account.feature:8'
+      overall_statistics.step_count_inc
+    end
   end
 end
